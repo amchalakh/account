62420c5a20f53c761e65ad42829ad26fcc43cb4b
Atul <Atul@DESKTOP-OULUVO9>
2019-04-02 19:26:14 +0530

    implementation of architecture is done

--- account/src/main/java/com/technotron/internetbanking/account/AccountApplication.java
+++ account/src/main/java/com/technotron/internetbanking/account/AccountApplication.java
@@ -2,8 +2,11 @@ package com.technotron.internetbanking.account;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+@ComponentScan(basePackages="com.technotron.*")
+@SpringBootApplication(exclude = HibernateJpaAutoConfiguration.class)
 
-@SpringBootApplication
 public class AccountApplication {
 
 	public static void main(String[] args) {
