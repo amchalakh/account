161003270438294e91c2177733569de4f4d586e9
manoj07hbti <60manojkumar@gmail.com>
2019-10-24 16:41:51 +0530

    first commit

--- account/src/main/java/com/technotron/internetbanking/account/AccountApplication.java
+++ account/src/main/java/com/technotron/internetbanking/account/AccountApplication.java
@@ -12,5 +12,5 @@ public class AccountApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(AccountApplication.class, args);
 	}
-
+    system.out.println("Changes in  GitHub Code");
 }
